469793a2dc8ac18fa088eeceb28ae1f33ba81e1a
Mehedi <mehedihasanmax95@gmail.com>
2022-03-22 19:08:02 +0600

    setting up firebase for web & mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone_flutter/responsive/responsive_layout.dart';
 import 'package:instagram_clone_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_clone_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // initialise app based on platform- web or mobile
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyC_oqe3l1Dl_ejFwlNnUpm8Bs88VkZ0kOo",
+        appId: "1:145742971412:web:dc7213af2883d3374c5d01",
+        messagingSenderId: "145742971412",
+        projectId: "instagram-clone-6d998",
+        storageBucket: "instagram-clone-6d998.appspot.com",
+      ),
+    );
+  }
+  else{
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
