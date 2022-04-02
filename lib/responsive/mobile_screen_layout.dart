d3508c29db78e7c35132cb9c9dd2a8b2d717f5be
Mehedi <mehedihasanmax95@gmail.com>
2022-04-02 22:41:27 +0600

    getting name from firebasefirestore

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,12 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  String username = "";
+  @override
+  void initState() {
+    super.initState();
+    getUserName();
+  }
+
+  void getUserName() async {
+    DocumentSnapshot snapshot = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+
+    setState(() {
+      // initially snapshot.data() contain all of the user data
+      // but in case we can't use snapshot.data()["userName"] directly
+      // instead of we have to convert snapshot.data() object as a Map
+
+      username = (snapshot.data() as Map<String, dynamic>)["userName"];
+      print(username);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Mobile Screen Layout")),
+    return Scaffold(
+      body: Center(child: Text("User Name is: $username")),
     );
   }
 }
